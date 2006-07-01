deb581840e5cbf4a32529c094e2da4bd14ccce53
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-01 22:21:45 +0000

    PolicyManager added

--- src/test/java/org/wyona/security/test/HelloWorld.java
+++ src/test/java/org/wyona/security/test/HelloWorld.java
@@ -1,5 +1,10 @@
 package org.wyona.security.test;
 
+import org.wyona.security.core.PolicyManagerFactory;
+import org.wyona.security.core.api.PolicyManager;
+
+import org.wyona.commons.io.Path;
+
 /**
  *
  */
@@ -10,5 +15,15 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        PolicyManagerFactory pmf = PolicyManagerFactory.newInstance();
+        PolicyManager pm = pmf.newPolicyManager();
+
+        Path path = new Path("/hello/world.html");
+        if (pm.authorize(path, null, null)) {
+            System.out.println("Access granted: " + path);
+        } else {
+            System.out.println("Access denied: " + path);
+        }
     }
 }
