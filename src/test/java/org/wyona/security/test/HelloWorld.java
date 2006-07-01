4de5bf174fe66eb6e3dda63d35160ace16ece69d
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-01 22:10:47 +0000

    test added

---
+++ src/test/java/org/wyona/security/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.wyona.security.test;
+
+/**
+ *
+ */
+public class HelloWorld {
+
+    /**
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
