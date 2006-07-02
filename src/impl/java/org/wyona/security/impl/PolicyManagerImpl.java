88420586117eaa6121f793b8b3ade56001476c65
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-02 10:06:12 +0000

    use yarep and log4j

--- src/impl/java/org/wyona/security/impl/PolicyManagerImpl.java
+++ src/impl/java/org/wyona/security/impl/PolicyManagerImpl.java
@@ -4,11 +4,29 @@ import org.wyona.commons.io.Path;
 import org.wyona.security.core.api.Identity;
 import org.wyona.security.core.api.PolicyManager;
 import org.wyona.security.core.api.Role;
+import org.wyona.yarep.core.RepositoryFactory;
+
+import org.apache.log4j.Category;
 
 /**
  *
  */
 public class PolicyManagerImpl implements PolicyManager {
+
+    private static Category log = Category.getInstance(PolicyManagerImpl.class);
+
+    private RepositoryFactory repoFactory;
+
+    /**
+     *
+     */
+    public PolicyManagerImpl() {
+        try {
+            repoFactory = new RepositoryFactory("ac-policies-yarep.properties");
+        } catch(Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 
     /**
      *
