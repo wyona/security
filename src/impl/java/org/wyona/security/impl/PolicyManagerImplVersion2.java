428831d51b0794efa1d9520dacc3d289c55ec1a5
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-10-25 14:07:53 +0000

    policy manager impl added with usecase element

---
+++ src/impl/java/org/wyona/security/impl/PolicyManagerImplVersion2.java
@@ -0,0 +1,220 @@
+package org.wyona.security.impl;
+
+import java.util.Hashtable;
+
+import org.wyona.commons.io.Path;
+import org.wyona.commons.io.PathUtil;
+import org.wyona.security.core.AuthorizationException;
+import org.wyona.security.core.api.Identity;
+import org.wyona.security.core.api.PolicyManager;
+import org.wyona.security.core.api.Role;
+import org.wyona.security.core.api.Usecase;
+import org.wyona.yarep.core.NoSuchNodeException;
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryFactory;
+import org.wyona.yarep.util.RepoPath;
+import org.wyona.yarep.util.YarepUtil;
+
+import org.apache.log4j.Category;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
+
+/**
+ *
+ */
+public class PolicyManagerImplVersion2 implements PolicyManager {
+
+    private static Category log = Category.getInstance(PolicyManagerImplVersion2.class);
+
+    private Repository policiesRepository;
+    private DefaultConfigurationBuilder configBuilder;
+
+    private static String USECASE_ELEMENT_NAME = "usecase";
+
+    /**
+     *
+     */
+    public PolicyManagerImplVersion2(Repository policiesRepository) {
+        this.policiesRepository = policiesRepository;
+        configBuilder = new DefaultConfigurationBuilder();
+    }
+    
+    /**
+     * Get policies repository of realm
+     */
+     public Repository getPoliciesRepository() {
+           return policiesRepository;
+     }
+         
+     
+    /**
+     * @deprecated
+     */
+    public boolean authorize(Path path, Identity identity, Role role) throws AuthorizationException {
+        return authorize(path.toString(), identity, role);
+       
+    }
+
+    /**
+     * @deprecated
+     */
+    public boolean authorize(String path, Identity identity, Role role) throws AuthorizationException {
+        log.warn("Deprecated method and not implemented!");
+        return false;
+    }
+   
+    /**
+     *
+     */
+    public boolean authorize(String path, Identity identity, Usecase usecase) throws AuthorizationException {
+        if(path == null || identity == null || usecase == null) {
+            log.error("Path or identity or usecase is null! [" + path + ", " + identity + ", " + usecase + "]");
+            throw new AuthorizationException("Path or identity or usecase is null! [" + path + ", " + identity + ", " + usecase + "]");
+        }
+
+        try {
+            return authorize(policiesRepository, path, identity, usecase);
+        } catch(Exception e) {
+            log.error(e.getMessage(), e);
+            throw new AuthorizationException("Error authorizing " + policiesRepository.getID() + 
+                    ", " + path + ", " + identity + ", " + usecase, e);
+        }
+    }
+
+    /**
+     *
+     */
+    private boolean authorize(Repository repo, String path, Identity identity, Usecase usecase) throws Exception {
+        if(repo == null) {
+            log.error("Repo is null!");
+            throw new Exception("Repo is null!");
+        } else if(path == null) {
+            log.error("Path is null!");
+            throw new Exception("Path is null!");
+        } else if(identity == null) {
+            log.error("Identity is null!");
+            throw new Exception("Identity is null!");
+        } else if(usecase == null) {
+            log.error("Usecase is null!");
+            throw new Exception("Usecase is null!");
+        }
+
+        String yarepPath = getPolicyPath(path); 
+        log.debug("Policy Yarep Path: " + yarepPath + ", Original Path: " + path + ", Repo: " + repo);
+        if (repo.existsNode(yarepPath)) {
+            try {
+                Configuration config = configBuilder.build(repo.getNode(yarepPath).getInputStream());
+                boolean useInheritedPolicies = config.getAttributeAsBoolean("use-inherited-policies", true);
+
+            Configuration[] usecases = config.getChildren(USECASE_ELEMENT_NAME);
+            for (int i = 0; i < usecases.length; i++) {
+                String usecaseName = usecases[i].getAttribute("id", null);
+                if (usecaseName != null && usecaseName.equals(usecase.getName())) {
+                    boolean useInheritedRolePolicies = usecases[i].getAttributeAsBoolean("use-inherited-policies", true);
+                    Configuration[] accreditableObjects = usecases[i].getChildren();
+
+                    boolean worldCredentialExists = false;
+                    boolean worldIsNotAuthorized = true;
+                    for (int k = 0; k < accreditableObjects.length; k++) {
+                        String aObjectName = accreditableObjects[k].getName();
+                        log.debug("Accreditable Object Name: " + aObjectName);
+
+                        if (aObjectName.equals("world")) {
+                            worldCredentialExists = true;
+                            String permission = accreditableObjects[k].getAttribute("permission", null);
+                            if (permission.equals("true")) {
+                                log.debug("Access granted: " + path);
+                                worldIsNotAuthorized = false;
+                                return true;
+                            } else {
+                                worldIsNotAuthorized = true;
+                            }
+                        } else if (aObjectName.equals("group")) {
+                            if (identity.getGroupnames() != null) {
+                                String groupName = accreditableObjects[k].getAttribute("id", null);
+                                String[] groupnames = identity.getGroupnames();
+                                if (groupnames != null) {
+                                    for (int j = 0; j < groupnames.length; j++) {
+                                        if (groupName.equals(groupnames[j])) {
+                                            String permission = accreditableObjects[k].getAttribute("permission", null);
+                                            if (permission.equals("true")) {
+                                                log.debug("Access granted: Path = " + path + ", Group = " + groupName);
+                                                return true;
+                                            } else {
+                                                log.debug("Access denied: Path = " + path + ", Group = " + groupName);
+                                                return false;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else if (aObjectName.equals("user")) {
+                            if (identity.getUsername() != null) {
+                                String userName = accreditableObjects[k].getAttribute("id", null);
+                                if (userName.equals(identity.getUsername())) {
+                                    String permission = accreditableObjects[k].getAttribute("permission", null);
+                                    if (permission.equals("true")) {
+                                        log.debug("Access granted: Path = " + path + ", User = " + userName);
+                                        return true;
+                                    } else {
+                                        log.debug("Access denied: Path = " + path + ", User = " + userName);
+                                        return false;
+                                    }
+                                }
+                            }
+                        } else if (aObjectName.equals("iprange")) {
+                            log.warn("Credential IP Range not implemented yet!");
+                            //return false;
+                        } else {
+                            log.warn("No such accreditable object implemented: " + aObjectName);
+                            //return false;
+                        }
+                    }
+                    if (worldCredentialExists && worldIsNotAuthorized) {
+                       log.debug("Access for world denied: " + path);
+                       return false;
+                    }
+                    if (!useInheritedRolePolicies){
+                        log.debug("Policy inheritance disabled for usecase:" + usecaseName + ". Access denied: "+ path);
+                        return false;
+                    }
+                }
+            }
+                if (!useInheritedPolicies) {
+                    log.debug("Policy inheritance disabled. Access denied: "+ path);
+                    return false;
+                }
+            } catch(NoSuchNodeException e) {
+                log.error(e.getMessage(), e);
+            }
+        } else {
+            if (yarepPath.equals("/.policy")) {
+                log.warn("No such node: " + yarepPath + " (" + repo + ")");
+            } else {
+                if (log.isDebugEnabled()) log.debug("No such node: " + yarepPath + " (Fallback to parent policy ...)");
+            }
+        }
+
+        String parent = PathUtil.getParent(path);
+        if (parent != null) {
+            // Check policy of parent in order to inherit credentials ...
+            log.debug("Check parent policy: " + parent + " ... (Current path: " + path + ")");
+            return authorize(repo, parent, identity, usecase);
+        } else {
+            log.warn("Trying to get parent of " + path + " (" + repo + ") failed. Access denied.");
+            return false;
+        }
+    }
+
+    /**
+     *
+     */
+    public String getPolicyPath(String path) {
+        // Remove trailing slash except for ROOT ...
+        if (path.length() > 1 && path.charAt(path.length() - 1) == '/') {
+            return path.substring(0, path.length() - 1) + ".policy";
+        }
+        return path + ".policy";
+    }
+}
