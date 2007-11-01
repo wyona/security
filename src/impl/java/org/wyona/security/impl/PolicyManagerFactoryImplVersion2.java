85506c5ee19280fd6b9e92cf1b5d85e011396679
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-11-01 22:51:06 +0000

    log that method is not implemented yet

--- src/impl/java/org/wyona/security/impl/PolicyManagerFactoryImplVersion2.java
+++ src/impl/java/org/wyona/security/impl/PolicyManagerFactoryImplVersion2.java
@@ -1,14 +1,19 @@
 package org.wyona.security.impl;
 
 import org.w3c.dom.Document;
+
 import org.wyona.security.core.PolicyManagerFactory;
 import org.wyona.security.core.api.PolicyManager;
 import org.wyona.yarep.core.Repository;
+
+import org.apache.log4j.Category;
 
 /**
  *
  */
 public class PolicyManagerFactoryImplVersion2 extends PolicyManagerFactory {
+
+    private static Category log = Category.getInstance(PolicyManagerFactoryImplVersion2.class);
 
     /**
      *
@@ -21,6 +26,7 @@ public class PolicyManagerFactoryImplVersion2 extends PolicyManagerFactory {
      *
      */
     public PolicyManager newPolicyManager(Document configuration, javax.xml.transform.URIResolver resolver) {
+        log.error("Not implemented yet!");
         return null;
     }
 }
