551dc2c46b611d3784b69a399362500fd50c1c3f
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2007-11-02 15:52:15 +0000

    implementation started

--- src/impl/java/org/wyona/security/impl/PolicyManagerFactoryImplVersion2.java
+++ src/impl/java/org/wyona/security/impl/PolicyManagerFactoryImplVersion2.java
@@ -5,8 +5,11 @@ import org.w3c.dom.Document;
 import org.wyona.security.core.PolicyManagerFactory;
 import org.wyona.security.core.api.PolicyManager;
 import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryFactory;
 
 import org.apache.log4j.Category;
+
+import java.io.File;
 
 /**
  *
@@ -27,6 +30,13 @@ public class PolicyManagerFactoryImplVersion2 extends PolicyManagerFactory {
      */
     public PolicyManager newPolicyManager(Document configuration, javax.xml.transform.URIResolver resolver) {
         log.error("Not implemented yet!");
-        return null;
+        String repoPath = configuration.getDocumentElement().getNodeValue();
+        log.error("DEBUG: Configuration: " + repoPath);
+        try {
+            return new PolicyManagerImplVersion2(new RepositoryFactory().newRepository("hugo", new File(repoPath)));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return null;
+        }
     }
 }
