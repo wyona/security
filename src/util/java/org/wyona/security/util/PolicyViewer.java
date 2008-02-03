62a8007fe20a0cb8f1c869565dfc8b3e3a9cde6c
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-02-03 21:55:58 +0000

    splitted path added

--- src/util/java/org/wyona/security/util/PolicyViewer.java
+++ src/util/java/org/wyona/security/util/PolicyViewer.java
@@ -15,12 +15,41 @@ public class PolicyViewer {
         try {
             Policy p = pm.getPolicy(path);
             if (p != null) {
-                return "<html><body>" + p.toString() + "</body></html>";
+                StringBuffer sb = new StringBuffer("<html><body>");
+                sb.append("<p>Access Policies for Path <i>" + path + "#" + contentItemId + "</i>:</p>");
+                sb.append("<p><table border=\"1\">");
+                sb.append("<tr><td>Path</td>" + getSplittedPath(pm, path, contentItemId) + "</tr>");
+                sb.append("<tr><td>Policy</td>" + getSplittedPath(pm, path, contentItemId) + "</tr>");
+                sb.append("<tr><td>Aggregated Policy</td>" + getSplittedPath(pm, path, contentItemId) + "</tr>");
+                sb.append("</table></p>");
+                //sb.append(p.toString());
+                sb.append("</body></html>");
+                return sb.toString();
             } else {
                 return "<html><body>No policy for path: " + path + "#"+contentItemId+"</body></html>";
             }
         } catch(Exception e) {
             return "<html><body>Exception: " + e.getMessage() + "</body></html>";
         }
+    }
+
+    /**
+     * Get splitted path
+     */
+    static public StringBuffer getSplittedPath (PolicyManager pm, String path, String contentItemId) {
+        String[] names = path.split("/");
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < names.length -1; i++) {
+            sb.append("<td>" + names[i] + "/</td>");
+        }
+        if (path.endsWith("/")) {
+            sb.append("<td>" + names[names.length -1] + "/</td>");
+        } else {
+            sb.append("<td>" + names[names.length -1] + "</td>");
+        }
+        if (contentItemId != null) {
+            sb.append("<td>#" + contentItemId + "</td>");
+        }
+        return sb;
     }
 }
