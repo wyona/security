c77f02999e7cca768cb868d875209b3d37b9c5f0
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2008-03-11 00:16:02 +0000

    getIdentityPolicies introduced

--- src/core/java/org/wyona/security/core/UsecasePolicy.java
+++ src/core/java/org/wyona/security/core/UsecasePolicy.java
@@ -5,14 +5,18 @@ import org.wyona.security.core.GroupPolicy;
 
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  */
 public class UsecasePolicy {
 
+    private static Logger log = Logger.getLogger(UsecasePolicy.class);
+
     private String name;
 
-    private Vector identities = null;
+    private Vector idps = null;
     private Vector gps = null;
 
     /**
@@ -20,7 +24,7 @@ public class UsecasePolicy {
      */
     public UsecasePolicy(String name) {
         this.name = name;
-        identities = new Vector();
+        idps = new Vector();
         gps = new Vector();
     }
 
@@ -34,19 +38,30 @@ public class UsecasePolicy {
     /**
      *
      */
-    public void addIdentity(Identity identity) {
-        identities.add(identity);
+    public void addIdentity(Identity identity, boolean permission) {
+        idps.add(new IdentityPolicy(identity, permission));
     }
 
     /**
      *
      */
     public Identity[] getIdentities() {
-        Identity[] ids = new Identity[identities.size()];
+        Identity[] ids = new Identity[idps.size()];
         for (int i = 0; i < ids.length; i++) {
-            ids[i] = (Identity) identities.elementAt(i);
+            ids[i] = ((IdentityPolicy) idps.elementAt(i)).getIdentity();
         }
         return ids;
+    }
+
+    /**
+     *
+     */
+    public IdentityPolicy[] getIdentityPolicies() {
+        IdentityPolicy[] ip = new IdentityPolicy[idps.size()];
+        for (int i = 0; i < ip.length; i++) {
+            ip[i] = (IdentityPolicy) idps.elementAt(i);
+        }
+        return ip;
     }
 
     /**
