811367b24a0268ccb84bdb053f02390ca07fd228
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-06-24 07:29:23 +0000

    cache made configurable

--- src/impl/java/org/wyona/security/impl/yarep/YarepGroupManager.java
+++ src/impl/java/org/wyona/security/impl/yarep/YarepGroupManager.java
@@ -33,7 +33,11 @@ public class YarepGroupManager implements GroupManager {
     
     private Repository identitiesRepository;
     protected UserManager userManager;
-    protected HashMap groups;
+
+    //private boolean cacheEnabled = true;
+    private boolean cacheEnabled = false;
+    private HashMap cachedGroups;
+    //protected HashMap groups;
 
     /**
      * Constructor.
@@ -41,29 +45,30 @@ public class YarepGroupManager implements GroupManager {
      * @param identitiesRepository
      * @throws AccessManagementException
      */
-    public YarepGroupManager(IdentityManager identityManager, Repository identitiesRepository) throws AccessManagementException {
+    public YarepGroupManager(IdentityManager identityManager, Repository identitiesRepository, boolean cacheEnabled) throws AccessManagementException {
         this.userManager = identityManager.getUserManager();
         this.identitiesRepository = identitiesRepository;
+        this.cacheEnabled = cacheEnabled;
     }
 
     /**
      * Finds all group nodes in the repository and instantiates the groups. 
      * @throws AccessManagementException
      */
-    public void loadGroups() throws AccessManagementException {
-        log.info("Load groups from repository '" + identitiesRepository.getName() + "'");
-        this.groups = new HashMap();
+    private Group[] loadGroupsFromRepository() throws AccessManagementException {
+        log.warn("DEBUG: Load groups from repository '" + identitiesRepository.getName() + "'");
         try {
             Node groupsParentNode = getGroupsParentNode();
             Node[] groupNodes = groupsParentNode.getNodes();
             DefaultConfigurationBuilder configBuilder = new DefaultConfigurationBuilder(true);
+            java.util.List<Group> groups = new java.util.ArrayList<Group>();
             for (int i = 0; i < groupNodes.length; i++) {
                 if (groupNodes[i].isResource()) {
                     try {
                         Configuration config = configBuilder.build(groupNodes[i].getInputStream());
                         if (config.getName().equals(YarepGroup.GROUP)) {
                             Group group = constructGroup(groupNodes[i]);
-                            this.groups.put(group.getID(), group);
+                            groups.add(group);
                         }
                     } catch (Exception e) {
                         String errorMsg = "Could not create group from repository node: " + groupNodes[i].getPath() + ": " + e;
@@ -73,6 +78,7 @@ public class YarepGroupManager implements GroupManager {
                     }
                 }
             }
+            return (Group[])groups.toArray(new Group[groups.size()]);
         } catch (RepositoryException e) {
             String errorMsg = "Could not read groups from repository: " + e;
             log.error(errorMsg, e);
@@ -92,7 +98,11 @@ public class YarepGroupManager implements GroupManager {
             YarepGroup group = new YarepGroup(userManager, this, id, name);
             group.setNode(groupsParentNode.addNode(id + "." + SUFFIX, NodeType.RESOURCE));
             group.save();
-            this.groups.put(id, group);
+
+            if (cacheEnabled) {
+                loadGroupIntoCache(id);
+            }
+
             return group;
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -101,7 +111,8 @@ public class YarepGroupManager implements GroupManager {
     }
     
     /**
-     *
+     * Override in subclasses
+     * @param node Repository node of group
      */
     protected Group constructGroup(Node node) throws AccessManagementException{
         return new YarepGroup(userManager, this, node);
@@ -111,17 +122,31 @@ public class YarepGroupManager implements GroupManager {
      * @see org.wyona.security.core.api.GroupManager#existsGroup(java.lang.String)
      */
     public boolean existsGroup(String id) throws AccessManagementException {
-        if (this.groups.containsKey(id)) {
-            // Exists within memory cache
-            return true;
-        } else {
-            // Check if group exists within persitent repository
-            try {
-                return getGroupsParentNode().hasNode(id + "." + SUFFIX);
-            } catch(Exception e) {
-                log.error(e, e);
-                return false;
-            }
+        // Check the cache first
+        if (!existsWithinCache(id)) {
+            // Also check the repository
+            return existsWithinRepository(id);
+        }
+        return true;
+    }
+
+    /**
+     * Check if group exists within cache
+     */
+    private boolean existsWithinCache(String userId) {
+        if (cacheEnabled && cachedGroups!= null && cachedGroups.containsKey(userId)) return true;
+        return false;
+    }
+
+    /**
+     * Check whether group exists within persistent identities repository
+     */
+    private boolean existsWithinRepository(String id) throws AccessManagementException {
+        try {
+            return getGroupsParentNode().hasNode(id + "." + SUFFIX);
+        } catch(Exception e) {
+            log.error(e, e);
+            return false;
         }
     }
 
@@ -130,31 +155,48 @@ public class YarepGroupManager implements GroupManager {
      */
     public Group getGroup(String id) throws AccessManagementException {
         if (!existsGroup(id)) {
+            log.warn("No such group: " + id);
             return null;
         } else {
-            if (this.groups.containsKey(id)) {
-                return (Group) this.groups.get(id);
-            } else {
-                log.info("Group '" + id + "' exists within persistent repository, but not within memory yet! Will be loaded into memory ...");
-                try {
-                    log.info("Add group id '" + id + "' to hash map first in order to break loops!");
-                    this.groups.put(id, null); // TODO: Describe what exactly can go wrong here!
-                    Group group = constructGroup(getGroupsParentNode().getNode(id + "." + SUFFIX));
-                    this.groups.put(group.getID(), group);
-                    return (Group) this.groups.get(id);
-                } catch (Exception e) {
-                    log.error(e, e);
-                    throw new AccessManagementException(e.getMessage());
+            if (cacheEnabled) {
+                if (!existsWithinCache(id)) {
+                    loadGroupIntoCache(id);
                 }
+                log.warn("Get group '" + id + "' from cache.");
+                return (Group) cachedGroups.get(id);
+            } else {
+                log.warn("Get group '" + id + "' from repository.");
+                return getGroupFromPersistentRepository(id);
             }
         }
+    }
+
+    /**
+     * Get group from repository
+     */
+    private Group getGroupFromPersistentRepository(String id) throws AccessManagementException {
+        if (existsWithinRepository(id)) {
+            try {
+                return constructGroup(getGroupsParentNode().getNode(id + "." + SUFFIX));
+            } catch (Exception e) {
+                log.error(e, e);
+                throw new AccessManagementException(e.getMessage());
+            }
+        }
+        log.warn("No such group within persistent repository: " + id);
+        return null;
     }
 
     /**
      * @see org.wyona.security.core.api.GroupManager#getGroups()
      */
     public Group[] getGroups() throws AccessManagementException {
-        return (Group[]) this.groups.values().toArray(new Group[this.groups.size()]);
+        log.warn("This method does not scale well. Rather use an iterator!");
+        if (cacheEnabled && cachedGroups != null) {
+            return (Group[]) cachedGroups.values().toArray(new Group[cachedGroups.size()]);
+        } else {
+            return loadGroupsFromRepository();
+        }
     }
 
     /**
@@ -165,7 +207,10 @@ public class YarepGroupManager implements GroupManager {
             throw new AccessManagementException("Group " + id + " does not exist.");
         }
         Group group = getGroup(id);
-        this.groups.remove(id);
+   
+        if (cacheEnabled && existsWithinCache(id)) {
+            cachedGroups.remove(id);
+        }
         group.delete();
     }
 
@@ -183,4 +228,34 @@ public class YarepGroupManager implements GroupManager {
         return this.identitiesRepository.getNode("/");    
     }
 
+    /**
+     * Loads a specific group from persistance storage into memory
+     *
+     * @param id Group id
+     * @throws AccessManagementException
+     */
+    protected synchronized void loadGroupIntoCache(String id) throws AccessManagementException {
+        log.warn("DEBUG: Load group '" + id + "' from persistent repository '" + identitiesRepository.getName() + "' into cache.");
+        if (cachedGroups == null) {
+            log.warn("No groups yet within memory. Initialize groups hash map.");
+            cachedGroups = new HashMap();
+        }
+        if (cachedGroups.containsKey(id)) {
+            log.warn("Group '" + id + "' already exists within memory, but will be reloaded!");
+        } else {
+            log.warn("Group '" + id + "' does not exist wihtin memory yet, but will be loaded now!");
+        }
+
+        Group group = getGroupFromPersistentRepository(id);
+        if (group != null) {
+            cachedGroups.put(id, group);
+        }
+    }
+
+    /**
+     * Check whether cache is enabled
+     */
+    protected boolean isCacheEnabled() {
+        return cacheEnabled;
+    }
 }
