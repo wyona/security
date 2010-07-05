29d41747f881089a57dcc60b18edfa5f71d23629
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-07-05 19:49:22 +0000

    test get parent users added

--- src/test/java/org/wyona/security/test/YarepGroupImplTest.java
+++ src/test/java/org/wyona/security/test/YarepGroupImplTest.java
@@ -46,6 +46,21 @@ public class YarepGroupImplTest extends TestCase {
     }
 
     /**
+     * Test to get all parent groups of an individual user
+     */
+    public void testGetAllParentGroupsOfUser() throws Exception {
+        User user = identityManager.getUserManager().getUser("lenya");
+        assertNotNull(user);
+        assertEquals("lenya@wyona.org", user.getEmail());
+
+        Group[] parentGroups = user.getGroups(true);
+        assertEquals(parentGroups.length, 3);
+
+        String[] parentGroupIDs = user.getGroupIDs(true);
+        assertEquals(parentGroupIDs.length, 3);
+    }
+
+    /**
      * Test to migrate groups index of an individual user of a previous version
      */
     public void testMigrateGroupsIndex() throws Exception {
@@ -162,109 +177,4 @@ public class YarepGroupImplTest extends TestCase {
             assertNull(parentGroups);   
         }
     }
-
-
-
-
-
-
-
-/* TODO: Delete testgroup2 if it already exists
-    public void testGroupMembers() throws Exception {
-        GroupManager groupManager = identityManager.getGroupManager(); 
-        UserManager userManager = identityManager.getUserManager();
-        String id = "testgroup2";
-        String name = "Test Group 2";
-        Group group = groupManager.createGroup(id, name);
-        String userID1 = "user-test1";
-        User user1 = userManager.createUser(userID1, "Some Name 1", "foo1@bar.org", "pwd123");
-        String userID2 = "user-test2";
-        User user2 = userManager.createUser(userID2, "Some Name 2", "foo2@bar.org", "pwd123");
-        String userID3 = "user-test3";
-        User user3 = userManager.createUser(userID3, "Some Name 3", "foo3@bar.org", "pwd123");
-
-        group.addMember(user1);
-        group.addMember(user2);
-        group.addMember(user3);
-        group.save();
-        
-        Item[] items = group.getMembers();
-        assertEquals(3, items.length);
-        for (int i=0; i<items.length; i++) {
-            User user = (User)items[i];
-            assertTrue(group.isMember(user));
-            assertTrue(user.getName().startsWith("Some Name"));
-        }
-    }
-*/
-
-
-
-/*
-    public void testGetUserGroups() throws Exception {
-        String userID = "lenya";
-        String groupID = "editors";
-        User user = identityManager.getUserManager().getUser(userID);
-        assertNotNull(user);   
-        Group group = identityManager.getGroupManager().getGroup(groupID);        
-        assertTrue(group.isMember(user));     
-        Group[] userGroups = user.getGroups();
-        assertEquals(1,userGroups.length);
-        assertEquals(groupID , userGroups[0].getID());
-    }
-*/
-
-/* TODO: Delete testuser if it already exists
-    public void testAddUser() throws Exception {
-        UserManager userManager = identityManager.getUserManager(); 
-        String id = "testuser";
-        String name = "Test User";
-        String email = "test@wyona.org";
-        String password = "test123";
-        assertFalse("User already exists: " + id, userManager.existsUser(id));
-        User user = userManager.createUser(id, name, email, password);
-        assertTrue(userManager.existsUser(id));
-        assertNotNull(user);
-        assertEquals(id, user.getID());
-        assertEquals(email, user.getEmail());
-        assertEquals(name, user.getName());
-        assertTrue(user.authenticate(password));
-    }
-*/
-   
-/*
-    public void testGetGroups() throws Exception {
-        String groupID = "editors";
-        Group group = identityManager.getGroupManager().getGroup(groupID);
-        assertNotNull(group);
-        assertEquals("Editors", group.getName());
-    }
-*/
-
-/* TODO: Delete testgroup1 if it already exists
-    public void testAddGroup() throws Exception {
-        GroupManager groupManager = identityManager.getGroupManager(); 
-        String id = "testgroup1";
-        String name = "Test Group 1";
-        assertFalse("Group already exists: " + id, groupManager.existsGroup(id));
-        Group group = groupManager.createGroup(id, name);
-        assertTrue(groupManager.existsGroup(id));
-        assertNotNull(group);
-        assertEquals(id, group.getID());
-        assertEquals(name, group.getName());
-        
-        // add member:
-        UserManager userManager = identityManager.getUserManager();
-        String userID = "user789";
-        User user = userManager.createUser(userID, "Some Name", "foo@bar.org", "pwd123");
-        assertFalse(group.isMember(user));
-        group.addMember(user);
-        group.save();
-        assertTrue(group.isMember(user));
-        
-        // delete user:
-        userManager.removeUser(userID);
-        assertFalse(group.isMember(user));
-    }
-*/
 }
