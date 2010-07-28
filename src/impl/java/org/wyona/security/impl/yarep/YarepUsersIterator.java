ee1761e6aeb39b896b1c95f65813d51451ac3e48
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-07-28 08:58:54 +0000

    users iterator started

---
+++ src/impl/java/org/wyona/security/impl/yarep/YarepUsersIterator.java
@@ -0,0 +1,27 @@
+package org.wyona.security.impl.yarep;
+
+/**
+ *
+ */
+public class YarepUsersIterator implements java.util.Iterator {
+
+    /**
+     *
+     */
+    public boolean hasNext() {
+        return false;
+    }
+
+    /**
+     *
+     */
+    public Object next() {
+        return null;
+    }
+
+    /**
+     *
+     */
+    public void remove() {
+    }
+}
