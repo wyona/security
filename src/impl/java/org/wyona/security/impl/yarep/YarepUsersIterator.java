e54bbdd81514f111d648d966e49dfe63757488f1
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2010-08-31 11:19:04 +0000

    javadoc and log4j messages added

--- src/impl/java/org/wyona/security/impl/yarep/YarepUsersIterator.java
+++ src/impl/java/org/wyona/security/impl/yarep/YarepUsersIterator.java
@@ -1,27 +1,34 @@
 package org.wyona.security.impl.yarep;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  */
 public class YarepUsersIterator implements java.util.Iterator {
 
+    private static Logger log = Logger.getLogger(YarepUsersIterator.class);
+
     /**
-     *
+     * @see java.util.Iterator#hasNext()
      */
     public boolean hasNext() {
+        log.error("TODO: Not implemented yet!");
         return false;
     }
 
     /**
-     *
+     * @see java.util.Iterator#next()
      */
     public Object next() {
+        log.error("TODO: Not implemented yet!");
         return null;
     }
 
     /**
-     *
+     * @see java.util.Iterator#remove()
      */
     public void remove() {
+        log.error("TODO: Not implemented yet!");
     }
 }
